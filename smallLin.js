e30aad9695eec8398f7c622fd15bbd261b85cfb8
paulforknow <paul830305@gmail.com>
2021-08-08 19:18:08 +0800

    twice commit

---
+++ smallLin.js
@@ -0,0 +1,52 @@
+let tool__search = document.querySelector('.tool__search');
+tool__search.addEventListener('click', function() {
+    location.href = 'https://www.google.com.tw/?hl=zh_TW';
+
+});
+$(document).ready(function() {
+    $('.dropDown').hide();
+    $('.ham').on('click', function() {
+        $('header').addClass('none');
+        console.log(111);
+        $('.mobileMenu').addClass('open');
+
+
+    });
+
+
+    $('.subMenu ').hover(function() {
+        $('.dropDown').slideDown(200);
+
+    }, function() {
+        $('.dropDown').toggle();
+
+    });
+
+    $('.tool__member').hover(function() {
+        $('.tool__member').addClass('background-gredient ');
+    }, function() {
+        $('.tool__member').removeClass('background-gredient ');
+    });
+    $('.tool__kart').hover(function() {
+        $('.tool__kart').addClass('background-gredient-2 ');
+    }, function() {
+        $('.tool__kart').removeClass('background-gredient-2 ');
+    });
+
+
+});
+
+
+function open() {
+    $('.mobileMenu').addClass('open');
+};
+
+
+
+
+$(document).ready(function() {
+    $('.ham').on('click', open);
+    $('mobileMenu-cancel').click(function() {
+        $('mobileMenu').removeClass('open');
+    })
+});
